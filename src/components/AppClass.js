1841a1e8678ff6002cdc95970e3d1b32d718d326
Iris Yu <iris.yu0716@gmail.com>
2020-10-12 11:33:23 +0800

    類別型元件

---
+++ src/components/AppClass.js
@@ -0,0 +1,36 @@
+// 導入react函式庫
+import React from "react";
+
+// extends: 繼承React裡的Component類別
+class App extends React.Component {
+  // 建構式: 產生實體初始值
+  constructor() {
+    // 建構式第一行必要呼叫super
+    // super(): 呼叫上層類別(React.Component)的建構式
+    super();
+    // this.state 這個元件的狀態
+    this.state = {
+      // 設定初始值
+      total: 100,
+    };
+  }
+
+  // render的回傳值即為最後呈現在網頁上的元素
+  render() {
+    return (
+      <>
+        <h1>{this.state.total}</h1>
+        {/* setState: class型元件的內建方法 */}
+        {/* 讓total = this.state.total + 1 */}
+        <button onClick={() => this.setState({ total: this.state.total + 1 })}>
+          +1
+        </button>
+        <button onClick={() => this.setState({ total: this.state.total - 1 })}>
+          -1
+        </button>
+      </>
+    );
+  }
+}
+
+export default App;
