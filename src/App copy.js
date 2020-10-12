73424ae512c00e92a6176f84698ff989aa9198aa
Iris Yu <iris.yu0716@gmail.com>
2020-10-12 15:28:47 +0800

    update

---
+++ src/App copy.js
@@ -0,0 +1,20 @@
+// 從node_modules的"react"導入React和useState函式庫
+// 在node_modules裡面就不需要寫路徑
+import React, { useState } from "react";
+
+// 導入元件
+import AppFuncion from "./components/AppFunction";
+import AppClass from "./components/AppClass";
+
+function App() {
+  return (
+    <>
+      {/* 使用類似HTML標記的方式來放置元件要呈現(渲染)的位置 */}
+      {/* 使用類似HTML給定屬性值(html屬性:attribute, React屬性叫props)的方式來傳遞資料到子女元件中 */}
+      <AppFuncion title="函式型元件" />
+      <AppClass title="類別型元件" />
+    </>
+  );
+}
+// 輸出元件(函式)
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-// 從node_modules的"react"導入React和useState函式庫
-// 在node_modules裡面就不需要寫路徑
+//--------setState執行異步---------//
+
+// 導入其它的模組
 import React, { useState } from "react";
 
-// 導入元件
-import AppFuncion from "./components/AppFunction";
-import AppClass from "./components/AppClass";
-
 function App() {
+  // 呼叫useState勾子，設定初始化值為0
+  // 回傳一組getter(獲取值)和setter(設定值)陣列
+  const [total, setTotal] = useState(0);
+
+  // return 值即元件的 render 方法，只能有一個根元素進行render
+  // 多個元件需要用<>...</>(React.Fragment)包住
   return (
     <>
-      {/* 使用類似HTML標記的方式來放置元件要呈現(渲染)的位置 */}
-      {/* 使用類似HTML給定屬性值(html屬性:attribute, React屬性叫props)的方式來傳遞資料到子女元件中 */}
-      <AppFuncion title="函式型元件" />
-      <AppClass title="類別型元件" />
+      <h1>{total}</h1>
+      <button
+        onClick={() => {
+          const newTotal = total + 1;
+          // setState/setXXXX 執行是異步的
+          setTotal(newTotal);
+          localStorage.setItem("total", newTotal);
+        }}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => {
+          const newTotal = total - 1;
+          setTotal(newTotal);
+          localStorage.setItem("total", newTotal);
+        }}
+      >
+        -1
+      </button>
     </>
   );
 }
+
 // 輸出元件(函式)
 export default App;
