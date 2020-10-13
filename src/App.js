78fb5d3db03e6bf1b4a00e5db37e7b6d0334ea69
Iris Yu <iris.yu0716@gmail.com>
2020-10-13 11:25:06 +0800

    美金轉新台應用完成-單元件版本

--- src/App.js
+++ src/App.js
@@ -5,20 +5,36 @@ function App(props) {
   const [twd, setTwd] = useState(0);
   const [usd, setUsd] = useState(0);
 
+  // 注意，這種數字運算會去掉小數然後四捨五入，所以不夠精準，只作範例用
+  const usd2Twd = (usd) => (usd * 28.53).toFixed(2);
+  // const twd2Usd = (twd) => (twd > 0 ? (twd / 28.53).toFixed(2) : 0);
+  const twd2Usd = (twd) => (twd / 28.53).toFixed(2);
+
   return (
     <>
       新台幣：
       <input
         type="text"
         value={twd}
-        onChange={(event) => setTwd(event.target.value)}
+        onChange={(e) => {
+          // 因setXXX 會異步執行，先得到更動後的值
+          const newTwd = e.target.value;
+          // 再設定兩個需要設定的狀態值
+          setTwd(newTwd);
+          setUsd(twd2Usd(newTwd));
+        }}
       />
       <br />
       美金：
       <input
         type="text"
         value={usd}
-        onChange={(event) => setUsd(event.target.value)}
+        onChange={(e) => {
+          // 先得到更動後的值
+          const newUsd = e.target.value;
+          setUsd(newUsd);
+          setTwd(usd2Twd(newUsd));
+        }}
       />
     </>
   );
